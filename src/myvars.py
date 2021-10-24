2e33c4b677f5d4ccff0a32a00ec525710152a076
Kema-dev <01sludge_coaches@icloud.com>
2021-10-24 13:50:20 +0200

    Estimation and vars creation done

--- src/myvars.py
+++ src/myvars.py
@@ -1,6 +1,5 @@
-import csv
-
 THETA_LOCATION = '../data/vars.csv'
+BACKUP_LOCATION = '../data/vars.csv.backup'
 
 theta0 = 0
 theta1 = 0
@@ -9,8 +8,8 @@ class TooManyRows(Exception()):
     """Theta values file has more than 1 row"""
     pass
 
-class TooManyColumns(Exception()):
-    """Theta values file has more than 2 columns"""
+class WrongNumberComa(Exception()):
+    """Theta values file doesn\'t have one and only one coma"""
     pass
 
 def init(new):
@@ -18,26 +17,42 @@ def init(new):
 		try:
 			with open(THETA_LOCATION, 'w+') as file:
 				file.write('0,0')
+				file.close()
+				theta0 = 0
+				theta1 = 0
+				print('Theta file initialized')
 		except:
 			print('Error during initialization of theta values')
 	else:
 		try:
-			with open(THETA_LOCATION, "r") as file:
-				if (file.read() == ""):
-					file.close()
-					file = open(THETA_LOCATION, "w+")
-					file.write('0,0')
-					file.close()
-					print('File didn t exist, created and initialized it')
-					return
-				read = csv.reader(file, delimiter = ',')
-				if (len(list(read)) > 1):
-					raise TooManyRows()
-				elif (sum(1 for row in read) > 2):
-					raise TooManyColumns()
-				else:
-					print('file is ok')
-		except TooManyColumns:
-			print('Theta values file has more than 2 columns')	
-		except TooManyRows:
-			print('Theta values file has more than 1 row')
+			file = open(THETA_LOCATION, "r")
+		except:
+			file = open(THETA_LOCATION, "w+")
+			file.write('0,0')
+			file.close()
+			print('File didn\'t exist, created and initialized it')
+			return
+		text = file.read()
+		try:
+			if (text.count(',') != 1):
+				raise WrongNumberComa
+			elif (text.count('\n') != 0):
+				raise TooManyRows
+			else:
+				val = text.split(',')
+				theta0 = int(val[0])
+				theta1 = int(val[1])
+		except:
+			file = open(BACKUP_LOCATION, "w+")
+			file.write(text)
+			file.close()
+			file = open(THETA_LOCATION, "w+")
+			file.write('0,0')
+			file.close()
+			print('File was corrupted, created a new one and initialized it, backed up the corrupted file at ' + BACKUP_LOCATION)
+			theta0 = 0
+			theta1 = 0
+			print('Theta values set to default values, theta0 = ' + str(theta0) + ', theta1 = ' + str(theta1))
+			return
+		print('Theta values retrieved, theta0 = ' + str(theta0) + ' $, theta1 = ' + str(theta1) + ' $/km')
+		return theta0, theta1
